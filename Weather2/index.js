849d0477226544e60486eede0cacf611ebc3693d
ingole8888 <ingole8888@gmail.com>
2022-06-01 19:54:57 +0530

    weather

--- Weather2/index.js
+++ Weather2/index.js
@@ -1,56 +1,40 @@
 //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=${api_key}
 //const api_key="a73a718241e013d65ebf19421e4f9ef2";
-//let url=`api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&appid=a73a718241e013d65ebf19421e4f9ef2`
-function getData(){
-    let city=document.getElementById('city').value;
-    
-    const url=`api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&appid=a73a718241e013d65ebf19421e4f9ef2`
+//let url=`api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&appid=a73a718241e013d65ebf19421e4f9ef2`
 
-    fetch(url).then(function (res){
-        return res.json();
-    })
-    .then(function (res){
-        append(res)
-        console.log(res);
-    })
-    .catch(function(err){
-        console.log(err)
-    })
-} 
+var api_key ="f953aaca4143ec6990f87145b3ad8eeb"
+let getData = async() => {
+    try{
+        let city =document.getElementById("city").value
+        let url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&appid=${api_key}`
 
-function append(data){
-    let container=document.getElementById('container');
-    let map=document.getElementById('gmap_canvas')
-    container.innerHTML=null
+        let res = await fetch(url);
+        let data =await res.json();
+        append(data.list);
+        console.log(data.list);
+    }catch(err){
+        console.log(err);
+    }
+  };
 
-    let city=document.createElement('h1');
-    city.innerText=`City: ${data.name}`;
+  let append = (data) => {
+    let container = document.getElementById("show_items");
+    data.forEach(({ main: { temp_min }, main: { temp_max } }) => {
+      let div = document.createElement("div");
 
-    let min=document.createElement('h3')
-    min.innerText=`Min Temp: ${data.main.temp_min}`;
+      let img=document.createElement("img")
+      img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJeaz4zgOMOgF3U6HoPhgbajddjsEeOUNscQ&usqp=CAU"
+      img.setAttribute("id","image")
+      
+      let min=document.createElement('h6')
+    min.innerText=temp_min-273;
 
-    let max=document.createElement('h3')
-    max.innerText=`Max Temp: ${data.main.temp_max}`;
+    let max=document.createElement('h6')
+    max.innerText=temp_max-273;
 
-    let current=document.createElement('h3')
-    current.innerText=`Current temp: ${data.main.temp}`;
+    div.append(img,min,max);
+      container.append(div);
+    });
+  };
+  
 
-    let humidity=document.createElement('h3')
-    humidity.innerText=`Humidity: ${data.main.humidity}`;
-
-    let wind=document.createElement('h3')
-    wind.innerText=`Wind Speed: ${data.wind.speed}`;
-
-    let cloud=document.createElement('h3')
-    cloud.innerText=`Clouds: ${data.clouds.all}`;
-
-    let sunrise =document.createElement('h3')
-    sunrise.innerText=`Sunrise: ${data.sys.sunrise}`;
-
-    let sunset=document.createElement('h3')
-    sunset.innerText=`Sunset: ${data.sys.sunset}`;
-
-    container.append(city,min,max,current,humidity,wind,cloud,sunrise,sunset)
-
-    map.src=`https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`
-}
