0736eef68a71f8da244ae601350c7f051281d415
ingole8888 <ingole8888@gmail.com>
2022-06-08 13:26:33 +0530

    food app with debouncing

---
+++ export import file/componants/componant.js
@@ -0,0 +1,18 @@
+function navbar(){
+
+    return ` <h2>MEAL-DEAL</h2>
+    <h2 id='name1'></h2>
+    <button><a href="someother.html">random meal</a></button>
+    <button><a href="today.html">Recipe Of the Day</a></button>
+<div>
+<input type="text" id="input" placeholder="search">
+<button id="submit">search</button>
+</div>
+<h3><a href="signup.html">signup</a></h3>
+<h3><a href="login.html">login</a></h3>
+
+`
+}
+export default navbar
+
+// <h2 id='random'><a href="random.html">random meal</a></h2>
